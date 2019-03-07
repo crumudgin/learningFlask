4d2e8856fc012ba48dfb81d67a8612c823f1493c
Zac Chu <zac.chu@gmail.com>
2019-03-07 09:50:26 -0500

    added templates and forms

--- hello.py
+++ hello.py
@@ -1,11 +1,43 @@
-from flask import Flask
+from datetime import datetime
+
+from flask import Flask, render_template, session, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 
-@app.route("/")
+app.config["SECRET_KEY"] = "secret key"
+
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+class NameForm(FlaskForm):
+    name = StringField("What is your name?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return "<h1>Hello World!</h1>"
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        session["name"] = form.name.data
+        return redirect(url_for("index"))
+    return render_template("index.html", current_time=datetime.utcnow(), form=form, name=session.get("name"))
 
 @app.route("/user/<name>")
 def user(name):
-    return "<h1>Hello %s!</h1>" %name
+    return render_template("user.html", name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("500.html"), 500
+
+if __name__ == "main":
+    app.run()
